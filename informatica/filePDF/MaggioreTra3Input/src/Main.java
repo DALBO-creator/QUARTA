36f2352aad141949c68057dedb8d6d8ee3ed13e6
ALBO DAVIDE <albodavidexd@gmail.com>
2025-09-19 09:52:10 +0200

    aggiunta esercizi

---
+++ informatica/filePDF/MaggioreTra3Input/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        int b;
+        int c;
+        System.out.println("inserisci 3 interi");
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        if (a>b && a > c){
+            System.out.println(a);
+        }
+    }
+}
