830f8f164d6e7f298fa0d35cb3015679e853c7a9
ALBO DAVIDE <albodavidexd@gmail.com>
2025-10-24 09:53:26 +0200

    Add initial Java array exercise project setup
    Added project files for the es10-array Java exercise, including .gitignore, IntelliJ IDEA configuration, and a starter Main.java with array initialization. This sets up the environment for further development.

---
+++ informatica/studi/es10-array/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int numeri[] = new int[20];
+        for (int numero:numeri) {
+            numero = 
+
+        }
+
+    }
+}
