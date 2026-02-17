a7697c01d09f2473030f5065f12319d8fd9d55cc
ALBO DAVIDE <albodavidexd@gmail.com>
2026-02-17 15:11:59 +0100

    Add Abbonamento, Bandiere and CarteCoperte projects
    Add new JavaFX projects: Abbonamento and CarteCoperte (sources, controllers, Launcher, module-info, FXML and resources) and their Maven setup (pom.xml, .mvn wrapper, mvnw scripts). Rename/move BandiereFx to Bandiere, add Bandiere sources and images, and remove legacy BandiereFx sources. Add IDE config and .gitignore files for the new modules and update a VCS config in IndovinaEventi.

---
+++ informatica/esercizi-laboratorio/Bandiere/src/main/java/com/bandiere/bandiere/Opzioni.java
@@ -0,0 +1,31 @@
+package com.bandiere.bandiere;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Opzioni {
+    private String imgSrc;
+    private ArrayList<String> opzioni;
+    private String opzioneCorretta;
+
+    public Opzioni(String imgSrc, ArrayList<String> opzioni, String opzioneCorretta) {
+        this.imgSrc = imgSrc;
+        this.opzioni = opzioni;
+        this.opzioneCorretta = opzioneCorretta;
+    }
+
+    public boolean checkCorretta(String s) {
+        return s.equals(opzioneCorretta);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(opzioni);
+    }
+
+    public ArrayList<String> getOpzioni() {
+        return opzioni;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+}
