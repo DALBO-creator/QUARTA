b125e8adaa5f2535c21520d9a4f8b3ddfdb68618
ALBO DAVIDE <albodavidexd@gmail.com>
2026-02-21 09:57:33 +0100

    Refactor CodiceFiscale and add Test
    Update CodiceFiscale to use instance fields (remove parameterized calcolaCognome/calcolaNome/calcolaAnno) and add multiple helper methods: calcolaMese, calcoloGiornoSesso, calcoloComune (placeholder), generaPrimi15 and calcoloControllo (computes control character). Also add Test.java with a simple main to construct CodiceFiscale and print intermediate components. These changes move logic to instance-based computation and implement control-character calculation; comune currently returns a hardcoded code.

--- informatica/esercizi-laboratorio/esCodiceFiscale/src/main/java/com/albo/escodicefiscale/CodiceFiscale.java
+++ informatica/esercizi-laboratorio/esCodiceFiscale/src/main/java/com/albo/escodicefiscale/CodiceFiscale.java
@@ -23,7 +23,7 @@ public class CodiceFiscale {
 
     public HashSet<Character> vocali = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-    public String calcolaCognome(String cognome) {
+    public String calcolaCognome() {
     StringBuilder ris = new StringBuilder();
     cognome = cognome.toLowerCase();
 
@@ -56,7 +56,7 @@ public class CodiceFiscale {
     }
 
 
-    public String calcolaNome(String nome) {
+    public String calcolaNome() {
     StringBuilder ris = new StringBuilder();
     nome = nome.toLowerCase();
 
@@ -96,10 +96,90 @@ public class CodiceFiscale {
     return ris.toString();
     }
 
-    public String calcolaAnno(LocalDate data) {
-    int anno = data.getYear();
+    public String calcolaAnno() {
+    int anno = dataNascita.getYear();
     return String.valueOf(anno).substring(2);
     }
+
+    public String calcolaMese() {
+        String[] corrispondenza = {"A", "B", "C", "D", "E", "H", "L", "M", "P", "R", "S", "T"};
+        int mese = dataNascita.getMonthValue();
+        return corrispondenza[mese - 1];
+
+    }
+
+    public String calcoloGiornoSesso(){
+        Integer giorno = dataNascita.getDayOfMonth();
+        String giornoStr = giorno.toString();
+
+        if (sesso.equals("mascio")) {
+            return "0" + giornoStr;
+        }
+        else {
+            giorno += 40;
+            return giorno.toString();
+        }
+
+    }
+
+    public String calcoloComune(){
+        return "B157";
+    }
+
+    public String generaPrimi15() {
+        return calcolaCognome()
+                + calcolaNome()
+                + calcolaAnno()
+                + calcolaMese()
+                + calcoloGiornoSesso()
+                + calcoloComune();
+    }
+    public char calcoloControllo() {
+
+        String primi15 = generaPrimi15();
+        int somma = 0;
+
+        int[] valoriPari = {
+                0,1,2,3,4,5,6,7,8,9,
+                0,1,2,3,4,5,6,7,8,9,
+                10,11,12,13,14,15,16,17,18,19,
+                20,21,22,23,24,25
+        };
+
+        int[] valoriDispari = {
+                1,0,5,7,9,13,15,17,19,21,
+                1,0,5,7,9,13,15,17,19,21,
+                2,4,18,20,11,3,6,8,12,14,
+                16,10,22,25,24,23
+        };
+
+        for (int i = 0; i < 15; i++) {
+
+            char c = primi15.charAt(i);
+            int valore;
+
+            if (Character.isDigit(c)) {
+                valore = c - '0';
+            } else {
+                valore = c - 'A' + 10;
+            }
+
+            if ((i + 1) % 2 == 0) {
+                somma += valoriPari[valore];
+            } else {
+                somma += valoriDispari[valore];
+            }
+        }
+
+        int resto = somma % 26;
+
+        return (char) ('A' + resto);
+    }
+
+
+
+
+
 
     
 
---
+++ informatica/esercizi-laboratorio/esCodiceFiscale/src/main/java/com/albo/escodicefiscale/Test.java
@@ -0,0 +1,22 @@
+package com.albo.escodicefiscale;
+
+import java.time.LocalDate;
+
+public class Test {
+    public static void main(String[] args) {
+        String datastr = "2008-11-17";
+
+        LocalDate data =  LocalDate.parse(datastr);
+        CodiceFiscale cf = new CodiceFiscale("davide", "albo", data , "maschio", "brescia");
+        System.out.println(cf.calcolaCognome());
+        System.out.println(cf.calcolaNome());
+        System.out.println(cf.calcolaAnno());
+        System.out.println(cf.calcolaMese());
+        System.out.println(cf.calcoloGiornoSesso());
+        System.out.println(cf.calcoloComune());
+        System.out.println(cf.calcoloControllo());
+
+    }
+
+
+}
